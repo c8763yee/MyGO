c6df35e66df79d6ed8ff03c316a97ecf1da97a73
c8763yee <49835073+c8763yee@users.noreply.github.com>
2025-01-18 15:04:42 +0800

    Change API URL to tomorin

--- static/index.js
+++ static/index.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://c8763yee.mooo.com:9527/api';
+const API_URL = 'https://tomorin.mooo.com:9527/api';
 // const API_URL = 'http://127.0.0.1:8080/api';
 // 異步處理與錯誤反饋
 function showLoading() {
